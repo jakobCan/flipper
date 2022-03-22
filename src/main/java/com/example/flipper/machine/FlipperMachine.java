ddfd962eb7bf4c3c9b122a2ba886883129084208
illuminatus52 <78482467+illuminatus52@users.noreply.github.com>
2022-03-22 11:51:17 +0100

    adds scoreboard to flipper machine

--- src/main/java/com/example/flipper/machine/FlipperMachine.java
+++ src/main/java/com/example/flipper/machine/FlipperMachine.java
@@ -2,6 +2,7 @@ package com.example.flipper.machine;
 
 import com.example.flipper.machine.flipperElements.composite.Bumper;
 import com.example.flipper.machine.flipperElements.composite.FlipperElement;
+import com.example.flipper.machine.flipperElements.visitor.Scoreboard;
 import com.example.flipper.states.*;
 
 import java.beans.PropertyChangeListener;
@@ -10,77 +11,99 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class FlipperMachine {
-
-    private FlipperState noCredit;
-    private FlipperState ready;
-    private FlipperState playing;
-    private FlipperState end;
-    private FlipperState currentState;
-    
-    private final PropertyChangeSupport support;
-
-
-    private List<FlipperElement>flipperElements = new ArrayList<>();
-    
-    private int credit = 0;
-    private int ball = 0;
-
-    public FlipperMachine() {
-        support = new PropertyChangeSupport(this);
-
-        noCredit = new NoCreditState(this);
-        ready = new ReadyState(this);
-        playing = new PlayingState(this);
-        end = new EndState(this);
-
-        currentState = noCredit;
-        this.flipperElements.add(new Bumper());
-    }
-
-    public void setCurrentState(FlipperState newFlipperState){
-        support.firePropertyChange("state", this.currentState, newFlipperState);
-        currentState = newFlipperState;
-    }
-
-    public void setCredit(int credit){
-        this.credit = credit;
-    }
-
-    public int getCredit() {
-        return credit;
-    }
-
-    public void setBall(int ball){
-        this.ball = ball;
-    }
-
-    public int getBall() {
-        return ball;
-    }
-
-    public void insertCoin(){
-        currentState.insertCoin();
-    }
-
-    public void pressStart(){
-        currentState.pressStart();
-    }
-
-    public FlipperState changeToEndState(){
-        return getEnd();
-    }
-
-    public FlipperState getNoCredit(){ return noCredit; }
-    public FlipperState getReady(){ return ready; }
-    public FlipperState getPlaying(){ return playing; }
-    public FlipperState getEnd(){ return end; }
-
-    public void addPropertyChangeListener(PropertyChangeListener pcl) {
-        support.addPropertyChangeListener(pcl);
-    }
-
-    public void removePropertyChangeListener(PropertyChangeListener pcl) {
-        support.removePropertyChangeListener(pcl);
-    }
-
+	
+	// State pattern
+	private FlipperState noCredit;
+	private FlipperState ready;
+	private FlipperState playing;
+	private FlipperState end;
+	private FlipperState currentState;
+	
+	// Observer pattern
+	private final PropertyChangeSupport support;
+	
+	// Composite pattern
+	private Scoreboard scoreboard;
+	private List<FlipperElement> flipperElements = new ArrayList<>();
+	
+	private int credit = 0;
+	private int ball = 0;
+	
+	public FlipperMachine() {
+		support = new PropertyChangeSupport(this);
+		
+		noCredit = new NoCreditState(this);
+		ready = new ReadyState(this);
+		playing = new PlayingState(this);
+		end = new EndState(this);
+		currentState = noCredit;
+		this.scoreboard = new Scoreboard();
+//		this.flipperElements.add(new Bumper());
+	}
+	
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	
+	public int getCredit() {
+		return credit;
+	}
+	
+	public void setBall(int ball) {
+		this.ball = ball;
+	}
+	
+	public int getBall() {
+		return ball;
+	}
+	
+	public void insertCoin() {
+		currentState.insertCoin();
+	}
+	
+	public void pressStart() {
+		currentState.pressStart();
+	}
+	
+	public FlipperState changeToEndState() {
+		return getEnd();
+	}
+	
+	public FlipperState getNoCredit() {
+		return noCredit;
+	}
+	
+	public FlipperState getReady() {
+		return ready;
+	}
+	
+	public FlipperState getPlaying() {
+		return playing;
+	}
+	
+	public FlipperState getEnd() {
+		return end;
+	}
+	
+	public Scoreboard getScoreboard() {
+		return scoreboard;
+	}
+	
+	public void setScoreboard(Scoreboard scoreboard) {
+		this.scoreboard = scoreboard;
+	}
+	
+	public void setCurrentState(FlipperState newFlipperState) {
+		support.firePropertyChange("state", this.currentState, newFlipperState);
+		currentState = newFlipperState;
+	}
+	
+	public void addPropertyChangeListener(PropertyChangeListener pcl) {
+		support.addPropertyChangeListener(pcl);
+	}
+	
+	public void removePropertyChangeListener(PropertyChangeListener pcl) {
+		support.removePropertyChangeListener(pcl);
+	}
+	
 }
--- src/main/java/com/example/flipper/machine/flipperElements/visitor/Scoreboard.java
+++ src/main/java/com/example/flipper/machine/flipperElements/visitor/Scoreboard.java
@@ -1,4 +1,20 @@
 package com.example.flipper.machine.flipperElements.visitor;
 
+import com.example.flipper.machine.flipperElements.command.HitFlipperElement;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Scoreboard {
+	private List<HitFlipperElement> hitFlipperElements = new ArrayList<>();
+
+	public void saveHit(HitFlipperElement hitFlipperElement) {
+		hitFlipperElements.add(hitFlipperElement);
+	}
+
+	public void calculateScore() {
+		for (HitFlipperElement h : hitFlipperElements) {
+			h.execute();
+		}
+	}
 }
