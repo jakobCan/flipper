169b9f135c3b24c0ea66c733b2c26956b45e14b3
illuminatus52 <78482467+illuminatus52@users.noreply.github.com>
2022-03-23 14:59:19 +0100

    adds Mediator

---
+++ src/main/java/com/example/flipper/machine/flipperElements/mediator/Mediator.java
@@ -0,0 +1,7 @@
+package com.example.flipper.machine.flipperElements.mediator;
+
+import com.example.flipper.machine.flipperElements.composite.FlipperElement;
+
+public interface Mediator {
+	public void notify(FlipperElement sender);
+}
